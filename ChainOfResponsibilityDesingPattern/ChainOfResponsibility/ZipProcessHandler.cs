70d62fa40b6f45ad6334bd38cf0666f2d3a80a44
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2024-02-24 16:06:23 +0300

    ZipProcessHandler

---
+++ ChainOfResponsibilityDesingPattern/ChainOfResponsibility/ZipProcessHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.IO.Compression;
+
+namespace ChainOfResponsibilityDesingPattern.ChainOfResponsibility
+{
+    public class ZipProcessHandler<T> : ProcessHandler
+    {
+        public override object Handle(object o)
+        {
+            var excelMemoryStream = o as MemoryStream;
+
+            excelMemoryStream.Position = 0;
+
+            using (var zipStream = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(zipStream, ZipArchiveMode.Create))
+                {
+                    var zipFile = archive.CreateEntry($"{typeof(T).Name}.xlsx");
+
+                    using (var zipEntry = zipFile.Open())
+                    {
+                        excelMemoryStream.CopyTo(zipEntry);
+                    }
+                }
+                return base.Handle(zipStream);
+            }
+        }
+    }
+}
