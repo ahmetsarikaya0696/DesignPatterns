016e8bafebd1b452c105956ad17a32ec50ee3fc6
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2024-02-27 10:18:58 +0300

    ThirParty library eklendi ve bu library için yeni interface ve concrete oluşturuldu

---
+++ AdapterDesignPattern/Services/AdvancedImageProcess.cs
@@ -0,0 +1,24 @@
+﻿using LazZiya.ImageResize;
+using System.Drawing;
+
+namespace AdapterDesignPattern.Services
+{
+    public class AdvancedImageProcess : IAdvancedImageProcess
+    {
+        public void AddWatermarkImage(Stream stream, string text, string filePath, Color textColor, Color outlineColor)
+        {
+            using (var img = Image.FromFile(@"wwwroot\images\image-file.jpg"))
+            {
+                var tOps = new TextWatermarkOptions
+                {
+                    TextColor = textColor,
+
+                    OutlineColor = outlineColor
+                };
+
+                img.AddTextWatermark(text, tOps)
+                   .SaveAs(filePath);
+            }
+        }
+    }
+}
---
+++ AdapterDesignPattern/Services/IAdvancedImageProcess.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace AdapterDesignPattern.Services
+{
+    public interface IAdvancedImageProcess
+    {
+        void AddWatermarkImage(Stream stream, string text, string filePath, Color color, Color outlineColor);
+    }
+}
