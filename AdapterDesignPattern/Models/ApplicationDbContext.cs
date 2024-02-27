27178d046286394846bc769de35c03dddc6c4ccb
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2024-02-27 09:44:05 +0300

    AdapterDesignPattern projesi oluşturuldu

---
+++ AdapterDesignPattern/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdapterDesignPattern.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
